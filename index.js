cc65138e0277d4bdaca4640c5d2c0a30dd465b54
HP <20060339@stu.omu.edu.tr>
2023-02-24 01:02:55 +0300

    ed

--- index.js
+++ index.js
@@ -12,8 +12,13 @@ document.write(`1. CEVAP : ${yeniDizi.join(" ")}`);
 
 let dizi1 = [3,6,9,14,16];
 
-const yeniDizi1 = dizi1.some(function(sayi1) {
-  return sayi1 > 5 ;
-});
-  
-document.write(`2. CEVAP : ${yeniDizi1}`);
+dizi1.map((element)=>{
+  if(element > 5) {
+    document.write('Beşten büyük bir eleman mevcut.')
+    break
+  } 
+  else {
+    document.write('Beşten büyük bir eleman mevcut değil.')
+    break
+  }
+})
