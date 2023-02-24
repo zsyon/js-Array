1f5fc91053c768a2ffa26469818902505f94eb4d
HP <20060339@stu.omu.edu.tr>
2023-02-24 16:54:28 +0300

    edit

---
+++ new.js
@@ -0,0 +1,23 @@
+s = "javascriptloops"
+
+let string = ""
+let dizi = s.split("")
+    
+for(let i=0 ; i<dizi.length ; i++){
+  if(dizi[i] == "a" ){
+    console.log(dizi[i])
+  }
+  else if(dizi[i] == "e"){
+    console.log(dizi[i])
+  }
+  else if(dizi[i] == "i"){
+    console.log(dizi[i])
+  }
+  else if(dizi[i] == "o"){
+    console.log(dizi[i])
+  }
+  else if(dizi[i] == "u"){
+    console.log(dizi[i])
+  }
+}
+
