5e899228e378a92ac1fea9fc61a6d87d3998e2e0
HP <20060339@stu.omu.edu.tr>
2023-02-24 18:31:21 +0300

    add new exemple

--- new.js
+++ new.js
@@ -21,3 +21,15 @@ for(let i=0 ; i<dizi.length ; i++){
   }
 }
 
+
+for (var i = 0; i <= 10; i++) {
+  console.log("dış döngü: " + i);
+  for (var j = 0; j <= 5; j++) {
+    if (j == 3) {
+      break;
+    }
+    console.log("iç döngü" + j);
+  }
+ }
+ 
+
